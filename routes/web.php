0fed9845637c99de56ab38d8683c91772ff67697
Aditya Bhardwaj <adityabhardwaj311@gmail.com>
2023-11-27 03:34:50 +0530

    Update Category Data with the Image

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\CategoryController;
 
 /*
 |--------------------------------------------------------------------------
@@ -29,7 +30,8 @@ Route::middleware(['auth','isAdmin'])->group(function(){
         Route::get('categories', [App\Http\Controllers\Admin\CategoryController::class, 'index']);
         Route::get('add-category', [App\Http\Controllers\Admin\CategoryController::class, 'add']);
         Route::post('insert-category', [App\Http\Controllers\Admin\CategoryController::class, 'insert']);
-
+        Route::get('edit-prod/{id}', [CategoryController::class, 'edit']);
+        Route::put('update-category/{id}', [CategoryController::class, 'update']);
     // Route::get('/dashboard','Admin\FrontendController@index');
     //Route::get('categories','Admin\CategoryController@index');
 
