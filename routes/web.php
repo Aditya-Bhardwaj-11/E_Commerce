77a4dd4da78f3c901a4778843835fd9c6b080a6d
Aditya Bhardwaj <adityabhardwaj311@gmail.com>
2023-11-29 02:45:54 +0530

    Products edit update and delete

--- routes/web.php
+++ routes/web.php
@@ -28,9 +28,11 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 Route::middleware(['auth','isAdmin'])->group(function(){
     //Route::get('users/{id}', function ($id) {
         Route::get('/dashboard', [App\Http\Controllers\Admin\FrontendController::class, 'index']);
+
         Route::get('categories', [App\Http\Controllers\Admin\CategoryController::class, 'index']);
         Route::get('add-category', [App\Http\Controllers\Admin\CategoryController::class, 'add']);
         Route::post('insert-category', [App\Http\Controllers\Admin\CategoryController::class, 'insert']);
+        
         Route::get('edit-category/{id}', [CategoryController::class, 'edit']);
         Route::put('update-category/{id}', [CategoryController::class, 'update']);
         Route::get('delete-category/{id}', [CategoryController::class, 'destroy']);
@@ -39,8 +41,10 @@ Route::middleware(['auth','isAdmin'])->group(function(){
         Route::get('add-products' , [ProductController::class , 'add']);
         Route::post('insert-products', [ProductController::class , 'insert']);
 
-    // Route::get('/dashboard','Admin\FrontendController@index');
-    //Route::get('categories','Admin\CategoryController@index');
+        Route::get('edit-product/{id}', [ProductController::class, 'edit']);
+        Route::put('update-product/{id}', [ProductController::class, 'update']);
+        Route::get('delete-product/{id}', [ProductController::class, 'destroy']);
+
 
 });
 
